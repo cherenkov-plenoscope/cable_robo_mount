58b9cbbee7185c3afcb9e9f74e4eb66150356a94
sebastian <sebmuell@phys.ethz.ch>
2016-11-09 13:36:07 +0100

    import the Geometry class directly

--- reflector_study/reflector_study/__init__.py
+++ reflector_study/reflector_study/__init__.py
@@ -1,6 +1,6 @@
 from . import optical_geometry
 from . import space_frame_geometry
-from . import Geometry
+from .Geometry import Geometry
 from . import Factory
 from . import config
 from . import tools
