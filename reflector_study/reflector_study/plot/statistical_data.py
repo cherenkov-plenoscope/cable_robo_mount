c697c2c8fffc590916e072874eed460f25065160
sebastian <sebmuell@phys.ethz.ch>
2016-11-09 14:26:52 +0100

    this is already in the tools

--- reflector_study/reflector_study/plot/statistical_data.py
+++ reflector_study/reflector_study/plot/statistical_data.py
@@ -1,16 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def lengths_of_bars(bars, node_numbers_xyz):
-
-    lengths = []
-    for i in range(bars.shape[0]):
-        try:
-            start, end = node_numbers_xyz[i,0], node_numbers_xyz[i,1]
-            lengths.append(np.linalg.norm(np.absolute(end - start)))
-        except IndexError:
-            pass
-    return np.array(lengths)
 
 def plot_histogram_of_bars(lengths):
     try:
