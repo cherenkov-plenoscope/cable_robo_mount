fdfe014d576b234b12b04e28ea23b16939e11fde
sebastian <sebmuell@phys.ethz.ch>
2016-11-09 12:23:16 +0100

    have a tool selection for node and bar related operations

--- reflector_study/reflector_study/__init__.py
+++ reflector_study/reflector_study/__init__.py
@@ -3,4 +3,5 @@ from . import space_frame_geometry
 from . import Geometry
 from . import Factory
 from . import config
+from . import tools
 from . import plot
---
+++ reflector_study/reflector_study/tools.py
@@ -0,0 +1,21 @@
+def node_position(nodes, ijk):
+    return nodes[ijk[0], ijk[1], ijk[2]]
+
+
+def node_in_range(nodes, ijk):
+    i_valid = ijk[0] >= 0 and ijk[0] < nodes.shape[0]
+    j_valid = ijk[1] >= 0 and ijk[1] < nodes.shape[1]
+    k_valid = ijk[2] >= 0 and ijk[2] < nodes.shape[2]
+    return i_valid and j_valid and k_valid
+
+
+def bar_start_and_end_position(nodes, bar):
+    start = node_position(nodes, bar[0])
+    end = node_position(nodes, bar[1])
+    return start, end
+
+
+def bar_in_range(nodes, bar):
+    start_is_in_range = node_in_range(nodes, bar[0])
+    end_is_in_range = node_in_range(nodes, bar[1])
+    return start_is_in_range and end_is_in_range
