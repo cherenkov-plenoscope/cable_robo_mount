a248a25d1048ef0ea2fc956dfb824c4f3015c2b7
sebastian <sebmuell@phys.ethz.ch>
2016-11-09 16:07:05 +0100

    an xml bridge

--- reflector_study/reflector_study/__init__.py
+++ reflector_study/reflector_study/__init__.py
@@ -5,3 +5,4 @@ from . import factory
 from . import config
 from . import tools
 from . import plot
+from . import mctracer_bridge
--- reflector_study/reflector_study/mctracer_bridge/xml.py
+++ reflector_study/reflector_study/mctracer_bridge/xml.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 
+def float2str(numeric_value):
+    return "{:.9f}".format(numeric_value)
+
+
 def tuple3(vec):
-    return '['+str(vec[0])+','+str(vec[1])+','+str(vec[2])+']'
+    return '['+float2str(vec[0])+','+float2str(vec[1])+','+float2str(vec[2])+']'
 
 
-def mct_hexagonal_imaging_mirror_facet(
+def hexagonal_imaging_mirror_facet(
     name,
     position,
     rotation,
@@ -21,8 +25,8 @@ def mct_hexagonal_imaging_mirror_facet(
     xml+=           'reflection_vs_wavelength="'+reflection_vs_wavelength+'" '
     xml+=           'color="[75,75,75]"/>\n'
     xml+= '    <set_sphere_cap_hexagonal '
-    xml+=           'curvature_radius="'+str(curvature_radius)+'" '
-    xml+=           'outer_radius="'+str(outer_radius)+'"/>\n'
+    xml+=           'curvature_radius="'+float2str(curvature_radius)+'" '
+    xml+=           'outer_radius="'+float2str(outer_radius)+'"/>\n'
     xml+= '</sphere_cap_hexagonal>\n'
     return xml
 
@@ -31,7 +35,7 @@ def cylinder(name, start_pos, end_pos, radius, color):
     xml = '<cylinder>\n'
     xml+= '    <set_frame name="'+name+'" pos="[0,0,0]" rot="[0,0,0]"/>\n'
     xml+= '    <set_surface reflection_vs_wavelength="zero" color="'+color+'"/>\n'
-    xml+= '    <set_cylinder radius="'+str(radius)+'" start_pos="'+tuple3(start_pos)+'" end_pos="'+tuple3(end_pos)+'"/>\n'
+    xml+= '    <set_cylinder radius="'+float2str(radius)+'" start_pos="'+tuple3(start_pos)+'" end_pos="'+tuple3(end_pos)+'"/>\n'
     xml+= '</cylinder>\n'
     return xml
 
@@ -39,7 +43,7 @@ def cylinder(name, start_pos, end_pos, radius, color):
 def sphere(name, pos, radius, color, reflection_vs_wavelength):
     xml = '<sphere>\n'
     xml+= '    <set_frame name="'+name+'" pos="'+tuple3(pos)+'" rot="[0,0,0]"/>\n'
-    xml+= '    <set_surface reflection_vs_wavelength="'+reflection_vs_wavelength+'" color="'color'"/>\n'
-    xml+= '    <set_sphere radius="'+str(radius)+'"/>\n'
+    xml+= '    <set_surface reflection_vs_wavelength="'+reflection_vs_wavelength+'" color="'+color+'"/>\n'
+    xml+= '    <set_sphere radius="'+float2str(radius)+'"/>\n'
     xml+= '</sphere>\n'
     return xml
