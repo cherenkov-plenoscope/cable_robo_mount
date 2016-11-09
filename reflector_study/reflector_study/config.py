cb065466e05b92f7d5e4a50b39cfccedb1807ee5
sebastian <sebmuell@phys.ethz.ch>
2016-11-09 12:33:02 +0100

    have an example config

--- reflector_study/reflector_study/config.py
+++ reflector_study/reflector_study/config.py
@@ -1,5 +1,30 @@
 import json
 
+example = {
+    'optics': { 
+        'focal_length': 75.0,
+        'davies_cotton_over_parabola_ratio': 0.0,
+        'max_outer_radius': 25.0,
+        'min_inner_radius': 5.0,
+        'gap_between_facets': 0.02,
+        'facet_inner_hex_radius': 0.6},
+    'space_frame': {
+        'material': {
+            'yielding_point': 1337.0,
+        },
+        'bar':{
+            'outer_radius': 0.05,
+            'thickness': 0.001,
+        },
+        'mirror_facet_weight': {
+            'surface_weight': 25.0,
+            'actuator_weight': 5.0
+        },
+        'number_of_layers': 2,
+        'x_over_z_ratio': 1.2
+    }
+}
+
 def write(config, path):
     with open(path, 'w') as outfile:
         json.dump(config, outfile, indent=4)
