cee519b39cc116c4d980786d064fa20ef9a4de83
spiros <daglass@student.ethz.ch>
2016-11-18 16:13:48 +0100

    more features. New file for the statistics of the flat reflector. relative import added. histogram of bars created. resolving issues #5 #6.

--- reflector_study/reflector_study/flat_tools.py
+++ reflector_study/reflector_study/flat_tools.py
@@ -7,9 +7,3 @@ def bar_length(nodes, bar):
     start = nodes[bar[0]]
     end = nodes[bar[1]]
     return np.linalg.norm(end - start)
-
-def bars_length(nodes, bars):
-    bars_length = []
-    for i in range((bars.shape[0])):
-        bars_length.append(bar_length(nodes, bars[i]))
-    return bars_length
--- reflector_study/reflector_study/plot/__init__.py
+++ reflector_study/reflector_study/plot/__init__.py
@@ -1,6 +1,5 @@
 from .overview import overview
 from .statistics import histogram_bar_length
-from .statistics_flat import histogram_bar_length
 from .add2ax import add2ax_mirror_tripods
 from .add2ax import add2ax_bars
 from .add2ax import add2ax_fixtures
