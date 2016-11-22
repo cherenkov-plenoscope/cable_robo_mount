9611c9398ca5461a58dfe0fbaef8ff4846d769bd
sebastian <sebmuell@phys.ethz.ch>
2016-11-22 15:23:20 +0100

    make the tests run again

--- reflector_study/tools/__init__.py
+++ reflector_study/tools/__init__.py
@@ -1,5 +1,4 @@
 from .tools import bar_length
-from .tools import bars_length
-from .tools import find_angle_between_two_bars
+from .tools import angle_between_bars
 from .tools import list_of_angles_between_neighbouring_bars
 from . import non_flat
