f211b6c7ade00eba1af8f3241c53a52c95e29c35
sebastian <sebmuell@phys.ethz.ch>
2016-11-22 15:11:23 +0100

    use bars instead of bar indices

--- reflector_study/tools/tools.py
+++ reflector_study/tools/tools.py
@@ -11,9 +11,9 @@ def bars_length(nodes, bars):
         bars_length.append(bar_length(nodes, bars[i]))
     return bars_length
 
-def find_angle_between_two_bars(bar1, bar2, nodes, bars):
-    Vec1 = nodes[bars[bar1][1]] - nodes[bars[bar1][0]]
-    Vec2 = nodes[bars[bar2][1]] - nodes[bars[bar2][0]]
+def find_angle_between_two_bars(bar1, bar2, nodes):
+    Vec1 = nodes[bar1[1]] - nodes[bar1[0]]
+    Vec2 = nodes[bar2[1]] - nodes[bar2[0]]
     angle_in_rad = np.arccos(sum(Vec1.T * Vec2.T)/(np.linalg.norm(Vec1) * np.linalg.norm(Vec2)))
     angle_in_degrees = np.degrees(angle_in_rad)
     return angle_in_degrees
@@ -23,5 +23,7 @@ def list_of_angles_between_neighbouring_bars(joints, bars, nodes):
     shape = 0
     for i in range(len(joints)):
         for j in range(len(joints[i])-1):
-            end_list.append([find_angle_between_two_bars(bar1 = joints[i][j], bar2 = joints[i][j+1], nodes = nodes, bars = bars)])
+            bar1 = bars[joints[i][j]]
+            bar2 = bars[joints[i][j+1]]
+            end_list.append([find_angle_between_two_bars(bar1=bar1, bar2=bar2, nodes=nodes)])
     return end_list
