a5899925b563f3c67c2b08492d3ea6f323acbc49
sebastian <sebmuell@phys.ethz.ch>
2016-11-24 18:13:38 +0100

    adopt the HomTra to express the mirror facet orientations

--- reflector_study/mirror_alignment/mirror_alignment.py
+++ reflector_study/mirror_alignment/mirror_alignment.py
@@ -1,6 +1,6 @@
 import numpy as np
 from .. import tools
-
+from ..HomTra import HomTra
 
 def mean_distance_between(focal_point, facet_centers):
     sum_of_distances = 0.0
@@ -32,9 +32,44 @@ def PAP_offset_in_z(
     return focal_piont[2] - focal_length
 
 
-def optimal_mirror_alignment(reflector):
-    tripod_centers = tools.mirror_tripod_centers(reflector['nodes'], reflector['mirror_tripods'])
-    pa_plane_offset = estimate_principal_aperture_plane_offset_in_z(
-        reflector, 
-        tripod_centers)
-    focal_piont = np.array([0.0, 0.0, reflector['geometry'].focal_length])
+def ideal_mirror_facet_HomTra_in_Rframe(focal_piont, facet_center):
+    unit_z = np.array([0.0, 0.0, 1.0])
+
+    connection = focal_piont - facet_center
+    connection /= np.linalg.norm(connection)
+
+    rot_axis = np.cross(unit_z, connection)
+    angle_to_unit_z = np.arccos(np.dot(unit_z, connection))
+
+    ideal_angle = angle_to_unit_z/2.0
+
+    t = HomTra()
+    t.set_translation(facet_center)
+    t.set_rotation_axis_and_angle(rot_axis, ideal_angle)
+    return t
+
+
+def ideal_mirror_facet_HomTras_in_Rframe(reflector):
+    nodes = reflector['nodes']
+    tripods = reflector['mirror_tripods']
+    focal_length = reflector['geometry'].focal_length
+
+    tripod_centers = tools.mirror_tripod_centers(nodes, tripods)
+    facet_centers = tripod_centers.copy()
+    facet_centers[:,2] += reflector['geometry'].facet_inner_hex_radius/10.0
+
+    PAP_offset = PAP_offset_in_z(focal_length, facet_centers)
+
+    # Rframe is the reflector frame
+    focal_piont_Rframe = np.array([0.0, 0.0, focal_length + PAP_offset])
+
+    mirror_facet_HomTras_Rframe = []
+    for facet_center in facet_centers:
+        mirror_facet_HomTras_Rframe.append(ideal_mirror_facet_HomTra_in_Rframe(
+            focal_piont_Rframe,
+            facet_center))
+
+    return mirror_facet_HomTras_Rframe
+
+
+
