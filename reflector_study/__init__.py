9b15c4b883e7f88f3860cb10a9deef975ac48ddf
sebastian <sebmuell@phys.ethz.ch>
2016-11-29 18:53:11 +0100

    hotfix to avoid Comtypes (Windows only) import on a Linux machine

--- reflector_study/__init__.py
+++ reflector_study/__init__.py
@@ -8,6 +8,8 @@ from . import mirror_alignment
 from . import mctracer_bridge
 from . import flatten
 from . import tools
-from . import SAP2000_bridge
+from sys import platform
+if platform == "win32":
+	from . import SAP2000_bridge
 from .HomTra import HomTra
 
