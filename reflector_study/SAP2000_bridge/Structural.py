800af46dfa84fce01924ce46b5365ea1355ad586
spiros <daglass@student.ethz.ch>
2016-12-07 16:53:18 +0100

    enhancement with wind values

--- reflector_study/SAP2000_bridge/Structural.py
+++ reflector_study/SAP2000_bridge/Structural.py
@@ -13,6 +13,7 @@ class Structural(object):
         self.angle_from_zenith = config_dict['reflector']['position_from_zenith']
         self.facet_inner_hex_radius = config_dict['reflector']['facet']['inner_hex_radius']
         self._set_up_loading()
+        self._set_up_loading_scenarios(config_dict)
 
     def _set_up_loading(self):
         self.facet_perimeter = 4*np.sqrt(3)*self.facet_inner_hex_radius
@@ -20,6 +21,15 @@ class Structural(object):
         self.total_facet_surface_weight = self.facet_surface_weight / 100 #in kN/m**2
         self.total_facet_weight = self.total_facet_surface_weight * self.facet_surface_area #in kN
         self.tripod_nodes_weight = self.total_facet_weight / 3 + self.facet_actuator_weight / 100 #in kN
+
+    def _set_up_loading_scenarios(self, config_dict):
+        self.wind_direction= config_dict["load_scenario"]["wind"]["direction"]
+        self.wind_speed = config_dict["load_scenario"]["wind"]["speed"]
+        self.wind_terrain_factor = config_dict["load_scenario"]["wind"]["terrain_factor"]
+        self.wind_orography_factor = config_dict["load_scenario"]["wind"]["orography_factor"]
+        self.wind_K1_factor =config_dict["load_scenario"]["wind"]["K1"]
+        self.wind_CsCd_factor =config_dict["load_scenario"]["wind"]["CsCd"]
+        self.wind_density =config_dict["load_scenario"]["wind"]["wind_density"]
 
     def __repr__(self):
         info = 'Structural and loading assumptions'
