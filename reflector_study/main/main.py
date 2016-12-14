e088bd537840139466bd67cd722a47bdc34aeb73
spiros <daglass@student.ethz.ch>
2016-12-14 11:16:02 +0100

    tension ring in a new factory. reduced main and put everything in a function in the main factory

--- reflector_study/main/main.py
+++ reflector_study/main/main.py
@@ -1,41 +1,16 @@
 import time
 start = time.time()
 import reflector_study as rs
-import numpy as np
+
 """
 general imports
 """
 geometry = rs.Geometry(rs.config.example)
-#create reflector geometry
-reflector = rs.factory.generate_reflector(geometry)
-reflector_nodes = reflector["nodes"]
-reflector_fixtures = np.array([x for x in reflector["fixtures"] if x != -1])
-reflector_bars = reflector["bars"]
-mirror_tripods = reflector["mirror_tripods"]
-
-#create tension ring geometry
-
-#find inner nodes of tension ring (not the same with fixtures, as in fixtures there are nodes in the middle layers)
-tension_ring_inner_nodes = rs.tension_ring.tools_new.inner_tension_ring_nodes_indices(reflector_nodes, reflector_fixtures)
-#categorize the inner nodes according to the angle from y axis
-tension_ring_inner_nodes_categorized = rs.tension_ring.tools_new.radar_categorization(tension_ring_inner_nodes, reflector_nodes)
-#create the bars related to the inner nodes (3 sets, the two diagonals and the straight ones)
-bars_inner = rs.tension_ring.tools_new.bars_from_fixture(tension_ring_inner_nodes_categorized)
-
-#create the new nodes of the tension ring (this is an array with coordinates)
-tension_ring_new_nodes_coordinates= rs.tension_ring.tools_new.tension_ring_outter_nodes(geometry, tension_ring_inner_nodes_categorized, reflector_nodes)
-#append them to the existing nodes array(from the reflector)
-nodes = np.concatenate((reflector_nodes, tension_ring_new_nodes_coordinates), axis=0)
-#find the node indices (simply the last tension_ring_new_nodes_coordinates.shape[0] nodes)
-tension_ring_outter_nodes = np.arange(reflector_nodes.shape[0], nodes.shape[0])
-#categorize the outter nodes according to the angle from y axis
-tension_ring_outter_nodes_categorized = rs.tension_ring.tools_new.radar_categorization(tension_ring_outter_nodes, nodes)
-#create the bars related to the outter nodes (3 sets, the two diagonals and the straight ones)
-bars_outter = rs.tension_ring.tools_new.bars_from_fixture(tension_ring_outter_nodes_categorized)
-#create the bars inbetween
-bars_inbetween = rs.tension_ring.tools_new.bars_inbetween(tension_ring_inner_nodes_categorized, tension_ring_outter_nodes_categorized)
-#bring all bars together
-bars = np.concatenate((reflector_bars, bars_inner, bars_outter, bars_inbetween), axis= 0)
+total_geometry = rs.factory.generate_reflector_with_tension_ring(geometry)
+nodes = total_geometry["nodes"]
+bars = total_geometry["bars"]
+mirror_tripods = total_geometry["mirror_tripods"]
+fixtures = total_geometry["fixtures"]
 
 """
 dish rotation
@@ -51,7 +26,7 @@ initialize SAP2000 and make assigns
 structural = rs.SAP2000_bridge.Structural(rs.config.example)
 bridge = rs.SAP2000_bridge.Bridge(structural)
 #bridge._SapObject.Hide()
-#bridge._SapObject.Unhide()
+bridge._SapObject.Unhide()
 
 path= "C:\\Users\\Spiros Daglas\\Desktop\\asdf\\test1\\spr"
 bridge.save_model(path+".sdb")
@@ -60,7 +35,7 @@ rs.SAP2000_bridge.bridge_s2v.s2k_frames(bars, path)
 bridge._SapModel.File.OpenFile(path+".$2k")
 
 
-bridge.elastic_support_definition(reflector_fixtures)
+bridge.elastic_support_definition(fixtures)
 
 bridge.load_scenario_dead()
 bridge.load_scenario_facet_weight(mirror_tripods)
@@ -87,6 +62,7 @@ bring dish to original position and prepare for mctracing simulation
 """
 nodes_deformed = rs.SAP2000_bridge.HomTra_bridge_tools.get_nodes_zenith_position(nodes_deformed_rotated, homogenous_transformation)
 
+reflector = rs.factory.generate_reflector(geometry)
 reflector_deformed = reflector.copy()
 reflector_deformed["nodes"]= nodes_deformed[:reflector["nodes"].copy().shape[0]]
 
