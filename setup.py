6295e74f9fb4c259afb11a2f7036641d42677bfb
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-10-09 14:43:55 +0200

    include full readme

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(name='reflector_study',
     version='0.0',
     description='Simulating a segmented imaging reflector',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Spyridon Daglas, Sebastian Achim Mueller',
     author_email='spirosdag@gmail.com',
     url='',
