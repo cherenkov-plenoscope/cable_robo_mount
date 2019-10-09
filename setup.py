06b6bd89ea134e8cbe3c9472cd260def776b46b3
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-10-09 14:44:45 +0200

    bumb version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(name='reflector_study',
-    version='0.0',
+    version='0.0.1',
     description='Simulating a segmented imaging reflector',
     long_description=long_description,
     long_description_content_type='text/markdown',
