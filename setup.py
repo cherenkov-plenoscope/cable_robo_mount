f6ff0b8192e7698f4745d28a0c1723b934ad8fea
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-10-09 15:11:53 +0200

    goes in new line

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import setuptools
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setuptools.setup(name='reflector_study',
+setuptools.setup(
+    name='reflector_study',
     version='0.0.1',
     description='Simulating a segmented imaging reflector',
     long_description=long_description,
