59aa5c0a366243f7c23086173c6b6e66bb307531
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-08-14 15:14:33 +0200

    update merlic_c89 for it to be explored more

--- cable_robo_mount/merlict_c89_bridge.py
+++ cable_robo_mount/merlict_c89_bridge.py
@@ -1,10 +1,10 @@
 import numpy
 import json
 
+
 def angle_between(v1, v2):
-    return np.arccos(np.dot(v1, v2)/(
-        np.linalg.norm(v1)*
-        np.linalg.norm(v2)))
+    return np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2)))
+
 
 def list_f8(l):
     return [float(l[0]), float(l[1]), float(l[2])]
@@ -17,10 +17,11 @@ def cylinder(
     surface={"inner": 0, "outer": 1},
     user_id=1,
 ):
-    rotsym_axis = end_pos - start_pos;
+    rotsym_axis = end_pos - start_pos
     length = np.linalg.norm(rotsym_axis)
     assert(length >= 0)
-    position = start_pos + rotsym_axis/2.0;
+    rotsym_axis = rotsym_axis/length
+    position = (start_pos + end_pos)/2.0
 
     ez = np.array([0, 0, 1])
     theta = angle_between(rotsym_axis, ez)
@@ -32,66 +33,114 @@ def cylinder(
         angle = -theta
 
     return {
-      "type": "Cylinder",
-      "id": int(user_id),
-      "pos": list_f8(position),
-      "rot": {
-        "repr": "axis_angle",
-        "axis": list_f8(axis),
-        "angle": float(angle)},
-      "radius": float(radius),
-      "length": float(length),
-      "surface": surface
+        "type": "Cylinder",
+        "id": int(user_id),
+        "pos": list_f8(start_pos),
+        "rot": {
+            "repr": "axis_angle",
+            "axis": list_f8(axis),
+            "angle": float(angle)
+        },
+        "radius": float(radius),
+        "length": float(length),
+        "surface": surface
     }
 
 
 def bars_to_merlictC89(nodes, bars, bar_radius, surface):
     bar_dicts = []
     for i, bar in enumerate(bars):
-            start_pos = nodes[bar[0]]
-            end_pos = nodes[bar[1]]
-            bar_dicts.append(
-                cylinder(
-                    user_id=i+1,
-                    start_pos=start_pos,
-                    end_pos=end_pos,
-                    radius=bar_radius,
-                    surface=surface))
+        start_pos = nodes[bar[0]]
+        end_pos = nodes[bar[1]]
+        bar_dicts.append(
+            cylinder(
+                user_id=i+1,
+                start_pos=start_pos,
+                end_pos=end_pos,
+                radius=bar_radius,
+                surface=surface))
     return bar_dicts
 
 
 def reflector_to_scenery(reflector, bar_radius=0.05):
     sc = {}
     sc["functions"] = [
-        [
-            [200e-9, 0.0],
-            [1200e-9, 0.0]
-        ],
-        [
-            [200e-9, 1.0],
-            [1200e-9, 1.0]
-        ],
-    ]
-    sc["colors"] = [
-        [22, 9, 255],
-        [255, 91, 49],
-    ]
-    sc["surfaces"] = [
         {
-          "material": 100,
-          "medium_refraction": 1,
-          "medium_absorbtion": 0,
-          "boundary_layer_specular_reflection": 0,
-          "boundary_layer_diffuse_reflection": 1,
-          "color": 0
+            "name": "unity",
+            "values": [
+                    [200e-9, 1.0],
+                    [1200e-9, 1.0]
+                ]
         },
         {
-          "material": 100,
-          "medium_refraction": 1,
-          "medium_absorbtion": 0,
-          "boundary_layer_specular_reflection": 0,
-          "boundary_layer_diffuse_reflection": 1,
-          "color": 1
+            "name": "refraction_glass",
+            "values": [
+                    [200e-9, 1.49],
+                    [1200e-9, 1.49]
+                ]
+        },
+        {
+            "name": "+infinity",
+            "values": [
+                    [200e-9, 9e99],
+                    [1200e-9, 9e99]
+                ]
+        },
+        {
+            "name": "zero",
+            "values": [
+                    [200e-9, 0.0],
+                    [1200e-9, 0.0]
+                ]
+        }
+    ]
+    sc["colors"] = [
+        {"rgb": [22, 9, 255], "name": "blue"},
+        {"rgb": [255, 91, 49], "name": "red"},
+        {"rgb": [16, 255, 0], "name": "green"},
+        {"rgb": [23, 23, 23], "name": "grey"}
+    ]
+    sc["media"] = [
+        {
+            "name": "vacuum",
+            "refraction": "unity",
+            "absorbtion": "+infinity"
+        },
+        {
+            "name": "glass",
+            "refraction": "refraction_glass",
+            "absorbtion": "+infinity",
+        }
+    ]
+    sc["default_medium"] = "vacuum"
+    sc["surfaces"] = [
+        {
+            "name": "glass_blue",
+            "material": "Transparent",
+            "specular_reflection": "unity",
+            "diffuse_reflection": "unity",
+            "color": "blue"
+        },
+        {
+            "name": "glass_red",
+            "material": "Transparent",
+            "specular_reflection": "unity",
+            "diffuse_reflection": "unity",
+            "color": "red"
+        },
+        {
+            "name": "specular_mirror",
+            "material": "Phong",
+            "specular_reflection": "unity",
+            "diffuse_reflection": "zero",
+            "color": "green"
+        },
+        {
+            "name": "perfect_absorber",
+            "material": "Phong",
+            "specular_reflection": "zero",
+            "diffuse_reflection": "zero",
+            "color": "grey"
         }
     ]
     sc["children"] = [
@@ -100,16 +149,24 @@ def reflector_to_scenery(reflector, bar_radius=0.05):
             "id": 133337,
             "pos": [0, 0, 1],
             "rot": {"repr": "tait_bryan", "xyz": [0, 0, 0]},
-            "children" :  bars_to_merlictC89(
+            "children": bars_to_merlictC89(
                 nodes=reflector["nodes"],
                 bars=reflector["bars"],
                 bar_radius=bar_radius,
-                surface={"inner": 0, "outer": 1})
+                surface={
+                    "inner": {"medium": "glass", "surface": "glass_blue"},
+                    "outer": {"medium": "vacuum", "surface": "specular_mirror"}
+                }
+            )
         }
     ]
     return sc
 
+
 def write_reflector(reflector, path, bar_radius=0.05):
-    refl_dict = reflector_to_scenery(reflector=reflector, bar_radius=bar_radius)
+    refl_dict = reflector_to_scenery(
+        reflector=reflector,
+        bar_radius=bar_radius
+    )
     with open(path, "wt") as f:
         f.write(json.dumps(refl_dict))
