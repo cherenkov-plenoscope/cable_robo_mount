4a7fa98c4113fd7cb706d9c4f727c1647f06a936
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-09-29 23:54:10 +0200

    spherical caps

--- cable_robo_mount/optical_geometry.py
+++ cable_robo_mount/optical_geometry.py
@@ -19,3 +19,11 @@ def z_hybrid(distance_to_z_axis, focal_length, dc_over_pa=0.0):
     z_dc = z_davies_cotton(distance_to_z_axis, focal_length)
     z_pa = z_parabola(distance_to_z_axis, focal_length)
     return dc_over_pa*z_dc + (1 - dc_over_pa)*z_pa
+
+
+def z_sphere(distance_to_z_axis, curvature_radius):
+    assert curvature_radius >= distance_to_z_axis
+    return curvature_radius - np.sqrt(
+        curvature_radius ** 2 -
+        distance_to_z_axis ** 2
+    )
